2af6e4b6a9cc83e459bcf5cd0dca782c0a705c68
Anton Grechkin <grechkinanton70@yandex.ru>
2023-08-08 19:10:30 +0700

    fix: 2 itera. fix for test

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,6 @@
+const { default: mongoose } = require('mongoose');
 const Card = require('../models/card');
-const handlerError = require('../utils/handlerError');
+// const handlerError = require('../utils/handlerError');
 
 module.exports.addCard = (req, res) => {
   const { name, link } = req.body;
@@ -25,14 +26,19 @@ module.exports.getCards = (req, res) => {
 module.exports.deleteCard = (req, res) => {
   // if (req.params.cardId.length === 24) {
   Card.findByIdAndDelete(req.params.cardId)
+    .orFail()
     .then((card) => {
-      if (!card) {
-        res.status(404).send({ message: 'Карточка с таким _id не найдена' });
-        return;
-      }
-      res.send({ message: 'Карточка удалена' });
+      res.send(card);
     })
-    .catch((err) => handlerError(err, res));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(400).send({ message: 'Некоректный _id' });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 //    else {
 //     res.status(400).send({ message: 'Карточка с указанным _id не найдена' });
@@ -43,14 +49,19 @@ module.exports.likeCard = (req, res) => {
   // if (req.params.cardId.length === 24) {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .populate('owner')
+    .orFail()
     .then((card) => {
-      if (!card) {
-        res.status(404).send({ message: 'Карточка с таким _id не найдена' });
-        return;
-      }
       res.send(card);
     })
-    .catch((err) => handlerError(err, res));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(400).send({ message: 'Некоректный _id' });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 //   else {
 //     res.status(400).send({ message: 'Карточка с указанным _id не найдена' });
@@ -61,14 +72,19 @@ module.exports.dislikeCard = (req, res) => {
   // if (req.params.cardId.length === 24) {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .populate('owner')
+    .orFail()
     .then((card) => {
-      if (!card) {
-        res.status(404).send({ message: 'Карточка с таким _id не найдена' });
-        return;
-      }
       res.send(card);
     })
-    .catch((err) => handlerError(err, res));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(400).send({ message: 'Некоректный _id' });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 //   else {
 //     res.status(400).send({ message: 'Карточка с указанным _id не найдена' });
