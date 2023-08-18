44249619e68f28057ff3c845a30809b921e707b9
Anton Grechkin <grechkinanton70@yandex.ru>
2023-08-18 18:47:58 +0700

    fix: middlewares

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const errorHandler = require('./middlewares/error-handler');
+const { errorHandler } = require('./middlewares/error-handler');
+// const errorHandler = require('./middlewares/error-handler');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -12,7 +13,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
   max: 100, // можно совершить максимум 100 запросов с одного IP
 });
-
+// подключаем rate-limiter
 app.use(limiter);
 
 app.use(helmet());
@@ -28,6 +29,18 @@ app.use('/', require('./routes/index'));
 
 app.use(errors());
 
-// app.use(errorHandler);
+// app.use((err, req, res, next) => {
+//   // если у ошибки нет статуса, выставляем 500
+//   const { statusCode = 500, message } = err;
+//   res.status(statusCode).send({
+//     // проверяем статус и выставляем сообщение в зависимости от него
+//     message: statusCode === 500
+//       ? 'На сервере произошла ошибка'
+//       : message,
+//   });
+//   next();
+// });
+
+app.use(errorHandler);
 
 app.listen(PORT);
